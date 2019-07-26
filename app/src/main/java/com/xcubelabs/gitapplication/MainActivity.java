8d2b282eb21d8afaf399f2566178e5494efa66ed
Sanket <sanket.deshmukh@xcubelabs.com>
2019-07-26 16:48:27 +0530

    Crazy Branch3 Commit For UnderConstruction

--- app/src/main/java/com/xcubelabs/gitapplication/MainActivity.java
+++ app/src/main/java/com/xcubelabs/gitapplication/MainActivity.java
@@ -22,6 +22,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        setSupportActionBar(toolbar);
+
+        setSupportActionBar(toolbar);
+        setSupportActionBar(toolbar);setSupportActionBar(toolbar);
+        setSupportActionBar(toolbar);setSupportActionBar(toolbar);setSupportActionBar(toolbar);setSupportActionBar(toolbar);setSupportActionBar(toolbar);setSupportActionBar(toolbar);setSupportActionBar(toolbar);
+
+
 
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
